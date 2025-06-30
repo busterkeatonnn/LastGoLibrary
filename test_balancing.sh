bff04e8fe1e9afb336da15aa1692bb2a92306858
busterkeatonnn <eeffiimmkkaa48@yandex.ru>
2025-06-30 18:33:20 +0000

    fix

--- test_balancing.sh
+++ test_balancing.sh
@@ -6,7 +6,7 @@ echo "Отправка $requests запросов..."
 
 for i in $(seq 1 $requests)
 do
-    curl --insecure -s -I -H "Host: localhost" https://127.0.0.1/api/books | grep X-Upstream
+    curl --insecure -s -I -H "Host: localhost" https://185.225.35.180/api/books | grep X-Upstream
     sleep 0.1
 done
 
